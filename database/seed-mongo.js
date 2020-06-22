618b10c6a02f9ba1e15e5b8b5148badf47900087
Iris Martinez <irismartinez114@gmail.com>
2020-06-21 19:26:38 -0700

    fixing seed

--- database/seed-mongo.js
+++ database/seed-mongo.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 // };
 
 // var data = JSON.parse(fs.readFileSync( 'seedData.json', 'utf8'));
- var data2 = JSON.parse(fs.readFileSync( 'seedData2.json', 'utf8'));
-// var data3 = JSON.parse(fs.readFileSync( 'seedData3.json', 'utf8'));
+ //var data2 = JSON.parse(fs.readFileSync( 'seedData2.json', 'utf8'));
+var data3 = JSON.parse(fs.readFileSync( 'seedData3.json', 'utf8'));
 // var data4 = JSON.parse(fs.readFileSync( 'seedData4.json', 'utf8'));
 // var data5 = JSON.parse(fs.readFileSync( 'seedData5.json', 'utf8'));
 // var data6 = JSON.parse(fs.readFileSync( 'seedData6.json', 'utf8'));
@@ -33,7 +33,7 @@ const db = mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopolog
   });
 
   var Image = mongoose.model('images', imageSchema);
-  Image.insertMany(data2, (err, response) => {
+  Image.insertMany(data3, (err, response) => {
     if (err) {
       console.log('error', err);
     } else {
